a0919546e13053c5b8c933f3e63677172d587b58
aweika <kller@foxmail.com>
2024-04-28 19:13:12 +0800

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -29,22 +29,26 @@
 
 
 extern "Rust" {
-    #[no_mangle]
     fn my_demo_function(a: u32) -> u32;
-    #[no_mangle]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
+    #[no_mangle]
+    fn my_demo_function(a: u32) -> u32{
         a
+    }
+    #[no_mangle]
+    fn my_demo_function_alias(a: u32) -> u32{
+        my_demo_function(a)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    
 
     #[test]
     fn test_success() {
